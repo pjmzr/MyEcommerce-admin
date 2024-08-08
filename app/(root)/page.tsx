aef7680fef7a940c5c8098ef6ae0133594d6b70e
Pejman Zarepour <ark.royal212@gmail.com>
2024-08-08 16:09:50 +0330

    created modal component with shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <UserButton />
+      <Modal title="test" description="test" isOpen onClose={() => {}} >
+        hi
+      </Modal>
     </div>
   );
 }
